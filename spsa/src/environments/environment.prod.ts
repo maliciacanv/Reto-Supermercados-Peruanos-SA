88315d27744d8a39a4ccfbf2b6e263c14863baea
maliciacanv <cantenovictoriomaruja@gmail.com>
2019-06-03 18:36:26 -0500

    se agrego script de boottrap y se agrego firebase al proyecto

--- spsa/src/environments/environment.prod.ts
+++ spsa/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:  {
+    apiKey: "AIzaSyCxF-p7S7HIyop8kZ0uEfPGc15gipGl_Vo",
+    authDomain: "reto-spsa.firebaseapp.com",
+    databaseURL: "https://reto-spsa.firebaseio.com",
+    projectId: "reto-spsa",
+    storageBucket: "reto-spsa.appspot.com",
+    messagingSenderId: "749060743605",
+    appId: "1:749060743605:web:92218b83db9c5a44"
+  }
 };
--- spsa/src/environments/environment.ts
+++ spsa/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:  {
+    apiKey: "AIzaSyCxF-p7S7HIyop8kZ0uEfPGc15gipGl_Vo",
+    authDomain: "reto-spsa.firebaseapp.com",
+    databaseURL: "https://reto-spsa.firebaseio.com",
+    projectId: "reto-spsa",
+    storageBucket: "reto-spsa.appspot.com",
+    messagingSenderId: "749060743605",
+    appId: "1:749060743605:web:92218b83db9c5a44"
+  }
 };
 
 /*
